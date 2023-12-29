6b0333a7eec7aaecda064f35a4b8d626f10bb5f1
Florance Rithi <florancerithi25@gmail.com>
2023-12-29 19:52:54 +0530

    added game history

--- src/Components/StatusMessage.jsx
+++ src/Components/StatusMessage.jsx
@@ -1,7 +1,9 @@
-const StatusMessage = ({ winner, isXNext, squares }) => {
+const StatusMessage = ({ winner, gamingBoard }) => {
+  const { squares, isXNext } = gamingBoard;
+
   // every() method returns true or false result for the condition , applied to the whole array
   const noMovesLeft = squares.every(squareValue => squareValue !== null);
-
+  // Computed/derived value : here we didnt use a new state because , every time when the state is updated , the component will be re-rendered , and the isXNext  state variable will also be changed and it will be up-to-date and the next line will use the updated isXNext to find the next player
   const nextPlayer = isXNext ? 'X' : 'O';
 
   const renderStatusMessage = () => {
